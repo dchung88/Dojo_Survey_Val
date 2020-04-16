59db7eb1d1b383f5a5d7462940638635d59adcb7
Denny Chung <dennychung@Dennys-MacBook-Pro.local>
2020-04-15 22:52:10 -0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyVal.Models;
+
+namespace DojoSurveyVal.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        [Route("display")]
+        public IActionResult Display(Survey userSurvey)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Display", userSurvey);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        // [Route("display")]
+        // [HttpGet]
+        // public IActionResult Success()
+        // {
+        //     return View("Display");
+        // }
+
+    }
+}
